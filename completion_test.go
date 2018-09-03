25ef3c82d81b6f9915bfde4d9e239a8692d852f0
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-09-04 08:56:04 +0900

    Add test code for completion

---
+++ completion_test.go
@@ -0,0 +1,85 @@
+package selector
+
+import "testing"
+
+func TestCompletionNew(t *testing.T) {
+	patterns := []*completion{
+		{suggestions: []string{"test1", "test2", "test3"}, target: 0},
+		{suggestions: []string{}, target: -1},
+	}
+
+	for _, pattern := range patterns {
+		completion := newCompletion(pattern.suggestions)
+		if completion.target != pattern.target {
+			t.Errorf("output: %v, request: %v", completion, pattern)
+		}
+	}
+}
+
+func TestCompletionNext(t *testing.T) {
+	patterns := []struct {
+		completion    *completion
+		requestTarget int
+	}{
+		{
+			completion: &completion{
+				suggestions: []string{"test1", "test2", "test3"}, target: 0,
+			},
+			requestTarget: 1,
+		},
+		{
+			completion: &completion{
+				suggestions: []string{"test1", "test2", "test3"}, target: 2,
+			},
+			requestTarget: 0,
+		},
+		{
+			completion: &completion{
+				suggestions: []string{}, target: -1,
+			},
+			requestTarget: -1,
+		},
+	}
+
+	for _, pattern := range patterns {
+		pattern.completion.next()
+		if pattern.completion.target != pattern.requestTarget {
+			t.Errorf("output: %d, request: %d",
+				pattern.completion.target, pattern.requestTarget)
+		}
+	}
+}
+
+func TestCompletionPrevious(t *testing.T) {
+	patterns := []struct {
+		completion    *completion
+		requestTarget int
+	}{
+		{
+			completion: &completion{
+				suggestions: []string{"test1", "test2", "test3"}, target: 2,
+			},
+			requestTarget: 1,
+		},
+		{
+			completion: &completion{
+				suggestions: []string{"test1", "test2", "test3"}, target: 0,
+			},
+			requestTarget: 2,
+		},
+		{
+			completion: &completion{
+				suggestions: []string{}, target: -1,
+			},
+			requestTarget: -1,
+		},
+	}
+
+	for _, pattern := range patterns {
+		pattern.completion.previous()
+		if pattern.completion.target != pattern.requestTarget {
+			t.Errorf("output: %d, request: %d",
+				pattern.completion.target, pattern.requestTarget)
+		}
+	}
+}
