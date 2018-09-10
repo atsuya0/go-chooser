a78a9edb6dde4b66c9474bc93b89d086e86c8054
atsuya <37957375+atsuya0@users.noreply.github.com>
2018-09-10 10:57:46 +0900

    [clean] rename field

--- key.go
+++ key.go
@@ -3,7 +3,7 @@ package choice
 import "bytes"
 
 type key struct {
-	key  int
+	id   int
 	code []byte
 }
 
@@ -46,45 +46,45 @@ const (
 )
 
 var keys = []*key{
-	{key: controlA, code: []byte{0x1}},
-	{key: controlB, code: []byte{0x2}},
-	{key: controlC, code: []byte{0x3}},
-	{key: controlD, code: []byte{0x4}},
-	{key: controlE, code: []byte{0x5}},
-	{key: controlF, code: []byte{0x6}},
-	{key: controlG, code: []byte{0x7}},
-	{key: controlH, code: []byte{0x8}},
-	// tab: {key: controlI, code: []byte{0x9}},
-	// enter: {key: controlJ, code: []byte{0xa}},
-	{key: controlK, code: []byte{0xb}},
-	{key: controlL, code: []byte{0xc}},
-	{key: controlM, code: []byte{0xd}},
-	{key: controlN, code: []byte{0xe}},
-	{key: controlO, code: []byte{0xf}},
-	{key: controlP, code: []byte{0x10}},
-	{key: controlQ, code: []byte{0x11}},
-	{key: controlR, code: []byte{0x12}},
-	{key: controlS, code: []byte{0x13}},
-	{key: controlT, code: []byte{0x14}},
-	{key: controlU, code: []byte{0x15}},
-	{key: controlV, code: []byte{0x16}},
-	{key: controlW, code: []byte{0x17}},
-	{key: controlX, code: []byte{0x18}},
-	{key: controlY, code: []byte{0x19}},
-	{key: controlZ, code: []byte{0x1a}},
-	{key: escape, code: []byte{0x1b}},
-	{key: enter, code: []byte{0xa}},
-	{key: backspace, code: []byte{0x7f}},
-	{key: delete, code: []byte{0x1b, 0x5b, 0x33, 0x7e}},
-	{key: tab, code: []byte{0x9}},
-	{key: backTab, code: []byte{0x1b, 0x5b, 0x5a}},
+	{id: controlA, code: []byte{0x1}},
+	{id: controlB, code: []byte{0x2}},
+	{id: controlC, code: []byte{0x3}},
+	{id: controlD, code: []byte{0x4}},
+	{id: controlE, code: []byte{0x5}},
+	{id: controlF, code: []byte{0x6}},
+	{id: controlG, code: []byte{0x7}},
+	{id: controlH, code: []byte{0x8}},
+	// tab: {id: controlI, code: []byte{0x9}},
+	// enter: {id: controlJ, code: []byte{0xa}},
+	{id: controlK, code: []byte{0xb}},
+	{id: controlL, code: []byte{0xc}},
+	{id: controlM, code: []byte{0xd}},
+	{id: controlN, code: []byte{0xe}},
+	{id: controlO, code: []byte{0xf}},
+	{id: controlP, code: []byte{0x10}},
+	{id: controlQ, code: []byte{0x11}},
+	{id: controlR, code: []byte{0x12}},
+	{id: controlS, code: []byte{0x13}},
+	{id: controlT, code: []byte{0x14}},
+	{id: controlU, code: []byte{0x15}},
+	{id: controlV, code: []byte{0x16}},
+	{id: controlW, code: []byte{0x17}},
+	{id: controlX, code: []byte{0x18}},
+	{id: controlY, code: []byte{0x19}},
+	{id: controlZ, code: []byte{0x1a}},
+	{id: escape, code: []byte{0x1b}},
+	{id: enter, code: []byte{0xa}},
+	{id: backspace, code: []byte{0x7f}},
+	{id: delete, code: []byte{0x1b, 0x5b, 0x33, 0x7e}},
+	{id: tab, code: []byte{0x9}},
+	{id: backTab, code: []byte{0x1b, 0x5b, 0x5a}},
 }
 
 func getKey(b []byte) int {
 	// Predefined key
 	for _, key := range keys {
 		if bytes.Equal(key.code, b) {
-			return key.key
+			return key.id
 		}
 	}
 	// Displayable characters.(space and graphic character.)
