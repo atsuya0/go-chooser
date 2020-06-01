b096f6e87dde2b1283939b23daa01e50eaa09c82
tayusa <37957375+tayusa@users.noreply.github.com>
2020-06-02 01:01:48 +0900

    Put together

--- signal.go
+++ signal.go
@@ -42,12 +42,10 @@ func (c *chooser) handleSignals(exitCh chan int, winSizeCh winSizeCh, stopCh sto
 			return
 		case signal := <-ch:
 			switch signal {
-			case syscall.SIGINT:
+			case syscall.SIGINT, syscall.SIGQUIT:
 				exitCh <- 0
 			case syscall.SIGTERM:
 				exitCh <- 1
-			case syscall.SIGQUIT:
-				exitCh <- 0
 			case syscall.SIGWINCH:
 				if winSize, err := c.terminal.getWinSize(); err != nil {
 					winSizeCh.err <- err
